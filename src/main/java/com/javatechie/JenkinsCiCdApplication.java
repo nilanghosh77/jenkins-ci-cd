87f1b8e2aa687ead050ce8073da2a6350bdc3ae8
Nilanjan Ghosh <nilanjanghosh@Nilanjans-MacBook-Pro.local>
2025-11-10 13:01:50 -0800

    code refactor commit

--- src/main/java/com/javatechie/JenkinsCiCdApplication.java
+++ src/main/java/com/javatechie/JenkinsCiCdApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsCiCdApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinsCiCdApplication.class, args);
+        SpringApplication.run(JenkinsCiCdApplication.class, args);
 	}
 
     @GetMapping("/greetings/{name}")
